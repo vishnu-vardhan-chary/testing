a3d7cf5151a1fa17461bcc356a9589dd5b862a59
vishnu-vardhan-chary <imvishnuchary@gmail.com>
2025-03-17 19:58:47 +0530

    commiting

---
+++ jenkinsTest.java
@@ -0,0 +1,8 @@
+class Testing {
+    public static void main(String[] args){
+        System.out.println("---------------------------");
+        System.out.println("Welcome to Jenkins Testing");
+         System.out.println("---------------------------");
+
+    }
+}
